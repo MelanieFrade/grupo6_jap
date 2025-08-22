42c4fe0793992b08d0b8f7c64ede3b70f0ca32de
melaniefrade <melanie.frade@estudiantes.utec.edu.uy>
2025-08-22 00:56:15 -0300

    Estilo login e index, funcionalidad de login, redireccion y guardado de sesion

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
-
 const container = document.getElementById("container");
 const registerBtn = document.querySelector(".register-btn");
 const loginBtn = document.querySelector(".login-btn");
 
+// PARA EL CAMBIO EN LA PANTALLA DE LOGIN A REGISTRO
 registerBtn.addEventListener("click", () => {
   container.classList.add("active");
 });
@@ -11,24 +11,22 @@ loginBtn.addEventListener("click", () => {
   container.classList.remove("active");
 });
 
-document.getElementById("btnPrimario").addEventListener("click", function() {
-    let username = document.getElementById("username").value.trim();
-    let password = document.getElementById("password").value.trim();
-
-    if (username === "" || password === "") {
-        alert("Por favor, complete todos los campos.");
-        return;
-    }
-    if( username.length < 5 || password.length < 5) {
-        alert("El nombre de usuario y la contraseña deben tener al menos 5 caracteres.");
-        return;
-    }
-    localStorage.setItem("estaLogueado", "true");
-    window.location.href = "index.html"; 
-    
+// VALIDACIÓN LOGIN
+document.getElementById("btnLogin").addEventListener("click", function (e) {
+  e.preventDefault();
+  let username = document.getElementById("login-username").value.trim();
+  let password = document.getElementById("login-password").value.trim();
+  console.log(username + password);
+  if (username === "" || password === "") {
+    alert("Por favor, complete todos los campos.");
+    return;
+  }
+  if (username.length < 5 || password.length < 5) {
+    alert(
+      "El nombre de usuario y la contraseña deben tener al menos 5 caracteres."
+    );
+    return;
+  }
+  sessionStorage.setItem("estaLogueado", "true");
+  window.location.href = "index.html";
 });
-
-document.getElementById("btnSecundario").addEventListener("click", function() {
-    window.location.href = "register.html";
-});
-
