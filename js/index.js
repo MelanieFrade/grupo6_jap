8da91715ff321c6650020a6f7a66e817736b5d33
prosp-typeA <nicoprosventin@hotmail.com>
2025-09-06 20:43:18 -0300

    implementacion de filtrado en productos

--- js/index.js
+++ js/index.js
@@ -18,22 +18,3 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location = "products.html"
     });
 });
-function mostrarAlerta(message){
-    const alerta = document.getElementById("ejemplo");
-    alerta.innerHTML = `<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ${message}
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>`
-}
--- js/products.js
+++ js/products.js
@@ -1,64 +1,145 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let contenedorListado = document.getElementById("listado");
+const ORDER_ASC_BY_NAME = "AZ";
+const ORDER_DESC_BY_NAME = "ZA";
+const ORDER_BY_PROD_COUNT = "Cant.";
+let currentProductsArray = [];
+let currentSortCriteria = undefined;
+let minCount = undefined;
+let maxCount = undefined;
 
-  // OBTENGO LA CATEGORÍA SELECCIONADA
-  const catID = localStorage.getItem("catID");
-  let url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
+function sortProducts(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_NAME) {
+        result = array.sort(function(a, b) {
+            if ( a.name < b.name ){ return -1; }
+            if ( a.name > b.name ){ return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_DESC_BY_NAME) {
+        result = array.sort(function(a, b) {
+            if ( a.name > b.name ){ return -1; }
+            if ( a.name < b.name ){ return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_BY_PROD_COUNT) {
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.soldCount); 
+            let bCount = parseInt(b.soldCount);
 
-  // VER COMO CAMBIAR EL FETCH PARA QUE SE INDIQUE LA CATEGORIA A MOSTRAR
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
+    return result;
+}
 
-  fetch(url)
-    // Manejo de la respuesta del servidor
-    .then((response) => {
-      // promesa, para que haga cosas mientras espera que ocurra
-      if (!response.ok) {
-        // si la respuesta es errónea, da un mensaje
-        throw new Error("Hubo un error!");
-      }
-      // devuelve la respuesta
-      return response.json();
-    })
-    // se muestran los datos
-    .then((producto) => {
-      // console.log(producto);
-      // para recorrer la listqa de productos de la categoría indicada
-      const productos = producto.products;
-      // console.log(productos);
-      // se recorre el listado de productos y se muestra en el contenedor
-      productos.forEach((producto) => {
-        contenedorListado.innerHTML += `
-    <div class="list-group-item zoom-hover img-zoom" data-id="${producto.id}">
-      <div class="container-fluid p-1">
-          <div class="row g-3 align-items-center">
-              <div class="col-12 col-md-3 text-center">
-                <img src="${producto.image}" alt="${producto.name}" class="img-fluid rounded" style="width:100%; height:auto;" />
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between mb-2">
-                        <h4 class="mb-1 text-black fs-3">${producto.name}</h4>
-                        <small class="text-muted">Vendidos: ${producto.soldCount}</small>
+function showProductLists(){
+    let contenedorListado = document.getElementById("listado");
+    contenedorListado.innerHTML = ""; 
+
+    currentProductsArray.forEach((producto) => {
+        if (((minCount == undefined) || (minCount != undefined && parseInt(producto.cost) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(producto.cost) <= maxCount))) {
+            
+            contenedorListado.innerHTML += `
+                <div class="list-group-item zoom-hover img-zoom" data-id="${producto.id}">
+                    <div class="container-fluid p-1">
+                        <div class="row g-3 align-items-center">
+                            <div class="col-12 col-md-3 text-center">
+                                <img src="${producto.image}" alt="${producto.name}" class="img-fluid rounded" style="width:100%; height:auto;" />
+                            </div>
+                            <div class="col">
+                                <div class="d-flex w-100 justify-content-between mb-2">
+                                    <h4 class="mb-1 text-black fs-3">${producto.name}</h4>
+                                    <small class="text-muted">Vendidos: ${producto.soldCount}</small>
+                                </div>
+                                <p class="mb-1 fs-3">${producto.description}</p>
+                                <p class="text-black fs-4"><strong>Precio:</strong> $${producto.cost}</p>
+                            </div>
+                        </div>
                     </div>
-                    <p class="mb-1 fs-3">${producto.description}</p>
-                    <p class="text-black fs-4"><strong>Precio:</strong> $${producto.cost}</p>
                 </div>
-            </div>
-        </div>
-    </div>
-    `;
-      });
-      
-    })
-    .catch((error) => {
-      // si hay un error, se muestra en consola
-      contenedorListado.innerHTML = `<div class="alert alert-danger" role="alert">Error al cargar los productos: ${error.message}</div>`;
+            `;
+        }
     });
-    // GUARDO EL ID DEL PRODUCTO CLICKEADO
-      contenedorListado.addEventListener("click", (e) => {
+
+    contenedorListado.addEventListener("click", (e) => {
         let card = e.target.closest("[data-id]");
         if (card) {
-          let idSeleccionado = card.getAttribute("data-id");
-          localStorage.setItem("productoID", idSeleccionado);
-          window.location.href = "product-info.html"; // página de detalle
+            let idSeleccionado = card.getAttribute("data-id");
+            localStorage.setItem("productoID", idSeleccionado);
+            window.location.href = "product-info.html";
         }
-      });
+    });
+}
+
+function sortAndShowProducts(sortCriteria, productsArray){
+    currentSortCriteria = sortCriteria;
+
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
+    }
+
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+    showProductLists();
+}
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    const catID = localStorage.getItem("catID");
+    let url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
+
+    fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Hubo un error!");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            currentProductsArray = data.products;
+            showProductLists();
+        })
+        .catch((error) => {
+            document.getElementById("listado").innerHTML = `<div class="alert alert-danger" role="alert">Error: ${error.message}</div>`;
+        });
+
+    document.getElementById("sortAsc").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_ASC_BY_NAME);
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_DESC_BY_NAME);
+    });
+
+    document.getElementById("sortByCount").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_BY_PROD_COUNT);
+    });
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+        document.getElementById("rangeFilterCountMin").value = "";
+        document.getElementById("rangeFilterCountMax").value = "";
+        minCount = undefined;
+        maxCount = undefined;
+        showProductLists();
+    });
+
+    document.getElementById("rangeFilterCount").addEventListener("click", function(){
+        minCount = document.getElementById("rangeFilterCountMin").value;
+        maxCount = document.getElementById("rangeFilterCountMax").value;
+
+        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+            minCount = parseInt(minCount);
+        } else {
+            minCount = undefined;
+        }
+
+        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+            maxCount = parseInt(maxCount);
+        } else {
+            maxCount = undefined;
+        }
+
+        showProductLists();
+    });
 });
