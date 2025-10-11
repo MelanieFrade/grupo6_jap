90f79365bafa70475b896c3022ae7cb812ee972a
Nahuel Porta <nahuelportasuarez@gmail.com>
2025-10-10 21:36:15 -0300

    Seccion myprofile junto a css y js

---
+++ js/my-profile.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+     const nombreUsuario = sessionStorage.getItem("username");
+      if (nombreUsuario) {
+        document.getElementById("user-info").textContent = nombreUsuario;
+      }
+    });
+
+
+document.i
